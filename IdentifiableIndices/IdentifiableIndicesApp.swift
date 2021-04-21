e813b008e4d2cfbeea3c3e082042c157bec3a91b
Dave Reed <dave256@mac.com>
2021-04-21 16:05:31 -0400

    Initial Commit

---
+++ IdentifiableIndices/IdentifiableIndicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdentifiableIndicesApp.swift
+//  IdentifiableIndices
+//
+//  Created by David M Reed on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct IdentifiableIndicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
